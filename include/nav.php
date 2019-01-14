1f8888fa8185f39c045d350d09cee3f5d61b3ac1
[josubulus] <[damien.cambus@gmail.com]>
2019-01-14 06:58:36 +0100

    correction petites erreur nav prinsipal

--- include/nav.php
+++ include/nav.php
@@ -1,9 +1,6 @@
 <div class="nav">
   <div class="lienNav">
-    <a href="ajout_entreprise.php">Ajouter entreprise</a>
-  </div>
-  <div class="lienNav">
-    <a href="index.php">Classement</a>
+    <a href="index.php">Accueil</a>
   </div>
   <div class="lienNav">
     <a href="logout.php">logout</a>
